f1597f84e3b91ce4b6b703c9e0233f07c8c2670a
maigiakien5812-cpu <maigiakien5812@gmail.com>
2026-06-06 18:16:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,8 @@ def delete(filename):
     return redirect("/")
 
 
-app.run(
-    host="0.0.0.0",
-    port=5000
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
 )
